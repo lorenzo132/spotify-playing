632f96192b9270543dc3aa7c720ed2415380d3ff
“lorenzo132” <lhoorn4@gmail.com>
2024-09-08 16:11:57 +0200

    Revert "Update scripts.js"
    This reverts commit 70a1a336e57450f32141ae964e611016fd8d04ae.
    
    Revert "Update styles.css"
    
    This reverts commit d7e54707d276ff0e99dea6d9295abfcc17b3e66b.

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let pauseStartTime = null;
-
     function formatTime(ms) {
         const minutes = Math.floor(ms / 60000);
         const seconds = ((ms % 60000) / 1000).toFixed(0);
@@ -13,12 +11,9 @@ document.addEventListener("DOMContentLoaded", function() {
             .then(data => {
                 const songInfoElement = document.getElementById("song-info");
                 const noSongElement = document.getElementById("no-song");
-                const overlayElement = document.querySelector(".overlay");
 
                 if (data.playing) {
                     noSongElement.style.display = "none";
-                    overlayElement.style.display = "none";
-                    
                     const progressPercentage = (data.progressMs / data.durationMs) * 100;
                     songInfoElement.innerHTML = `
                         <img src="${data.albumArt}" alt="${data.albumName}">
@@ -32,16 +27,9 @@ document.addEventListener("DOMContentLoaded", function() {
                             </div>
                         </div>
                     `;
-                    pauseStartTime = null;
                 } else {
                     noSongElement.style.display = "block";
                     songInfoElement.innerHTML = "";
-                    
-                    if (pauseStartTime === null) {
-                        pauseStartTime = Date.now();
-                    } else if (Date.now() - pauseStartTime > 5000) { // 5 seconds in milliseconds
-                        overlayElement.style.display = "block";
-                    }
                 }
             })
             .catch(error => console.error("Error fetching song data:", error));
